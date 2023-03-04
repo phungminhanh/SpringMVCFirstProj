3d398e55a669dede855bb3af063efdc2392fd9cd
phunganh23082 <phunganh23082k@gmail.com>
2023-03-04 23:35:53 +0700

    new version

--- src/main/java/com/fsoft/dao/UserDaoImpl.java
+++ src/main/java/com/fsoft/dao/UserDaoImpl.java
@@ -9,34 +9,30 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import com.fsoft.bean.User;
+
 @Component
 public class UserDaoImpl implements IUserDao {
-@Autowired
-private JdbcTemplate jdbcTemplate;
-@Override
-		public User checkLogin(User user) {
-			String query ="select * from user where user_name = ? and password = ? ";
-			List<User> listUser = jdbcTemplate.query(query, new Object[] {user.getUserName(),user.getPassword()}, new UserRowMapper());
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	@Override
+	public User checkLogin(User user) {
+		String query = "select * from user where user_name = ? and password = ? ";
+		List<User> listUser = jdbcTemplate.query(query, new Object[] { user.getUserName(), user.getPassword() },
+				new UserRowMapper());
 
 		return listUser.get(0);
-			 
-		}
+
+	}
 }
-class UserRowMapper implements org.springframework.jdbc.core.RowMapper<User>{
-	
-	public  User mapRow(ResultSet resultSet, int iValue) throws SQLException
-	{
-     User user = new User();
-	 user.setUserName(resultSet.getString(2));
-	 user.setPassword(resultSet.getNString(4));
-	
-	return user;
+
+class UserRowMapper implements org.springframework.jdbc.core.RowMapper<User> {
+
+	public User mapRow(ResultSet resultSet, int iValue) throws SQLException {
+		User user = new User();
+		user.setUserName(resultSet.getString(2));
+		user.setPassword(resultSet.getNString(4));
+
+		return user;
 	}
-	}
-
-
-		
-
-
-
-
+}
